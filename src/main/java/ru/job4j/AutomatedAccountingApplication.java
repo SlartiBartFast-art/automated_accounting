529044c8339f2813d120d75e240060f9ea697035
SlartiBartFast-art <JekHamond@tutanota.com>
2022-03-17 14:44:14 +0500

    create automated_accounting commit

---
+++ src/main/java/ru/job4j/AutomatedAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedAccountingApplication.class, args);
+    }
+
+}
